b68c6a6ed33c93b009635931fa6aebadc438cd71
Prasad Yara <prasad.yara2779@gmail.com>
2025-12-24 14:13:58 +0530

    custom functional interfaces

---
+++ src/com/java/version/eight/streams/level/hard/dto/Employee.java
@@ -0,0 +1,24 @@
+package com.java.version.eight.streams.level.hard.dto;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return  name  +" : " + salary ;
+    }
+}
